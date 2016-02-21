85259f3b67acaf6d397a20a180cf033417ba7b4e
David J Peacock <david@davidjpeacock.ca>
2016-02-20 22:41:20 -0500

    fixed tests

--- conversionfunctions_test.go
+++ conversionfunctions_test.go
@@ -1,4 +1,4 @@
-package conversions
+package conversions_test
 
 import (
 	"testing"
@@ -8,18 +8,18 @@ import (
 
 func TestMetresToFeet(t *testing.T) {
 	t.Log("Converting metres to feet... (expected feet: 9.843)")
-	answer := conversions.MetresToFeet(3)
+	answer := conversions.MetreToFoot(3)
 
-	if answer != "9.843ft" {
-		t.Errorf("Expected 9.843ft, but got %g", answer)
+	if answer != 9.843 {
+		t.Errorf("Expected 9.843, but got %g", answer)
 	}
 }
 
 func TestFeetToMetres(t *testing.T) {
-	t.Log("Converting feet to metres... (expected metres: 2.743)")
-	answer := conversions.FeetToMetres(9)
+	t.Log("Converting feet to metres... (expected metres: 2.7430661383724475)")
+	answer := conversions.FootToMetre(9)
 
-	if answer != "2.743m" {
-		t.Errorf("Expected 2.743m, but got %g", answer)
+	if answer != 2.7430661383724475 {
+		t.Errorf("Expected 2.7430661383724475, but got %g", answer)
 	}
 }
